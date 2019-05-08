78acd1807ed8820352b567744f574770f606ba5d
Francisco Ruiz <paco1998@hotmail.es>
2019-05-08 16:14:49 +0200

    Añadida Pista

--- Track.js
+++ Track.js
@@ -6,17 +6,17 @@ class Track extends THREE.Mesh {
         var that = this;
 
         var loader = new THREE.OBJLoader2();
-        loader.loadMtl('FullTrack.mtl', null,
+        loader.loadMtl('src/FullTrack.mtl', null,
             function (materials) {
                 loader.setMaterials(materials);
                 loader.setLogging(true, true);
-                loader.load('Fulltrack.obj',
+                loader.load('src/Fulltrack.obj',
                     function (object) {
                         var modelo = object.detail.loaderRootNode;
                         that.add(modelo);
                     }, null, null, null, false);
             });
         
-            this.scale.set(10,10,10);
+            this.scale.set(20,20,20);
     }
 }
