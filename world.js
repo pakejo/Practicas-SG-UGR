62580513c046c3d46e36b0757d72c142e13cb4e6
zandrokaki <44413444+zandrokaki@users.noreply.github.com>
2019-05-17 03:50:10 +0200

    añadido primer tunel

--- world.js
+++ world.js
@@ -4,14 +4,29 @@ class World extends THREE.Mesh{
 
         this.geometry = new THREE.CubeGeometry(10000,10000,10000);
 
+        var buildingTexture = new THREE.TextureLoader().load('imgs/textura_edificio2.jpg');
+
+        var skyscraper1 = new THREE.Mesh();
+            skyscraper1.geometry = new THREE.CubeGeometry(140,150,60);
+            skyscraper1.material = new THREE.MeshPhongMaterial ({map: buildingTexture});
+
+        var hole1 = new THREE.CubeGeometry(140,20,40);
+
+        var hole1bsp = new ThreeBSP(hole1);
+        var skyscraper1bsp = new ThreeBSP(skyscraper1);
+        var building1 = skyscraper1bsp.subtract(hole1bsp);
+        
+
+
+
         this.texture = 
         [
-            new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('imgs/darkcity_ft.jpg'),side: THREE.DoubleSide}),
-            new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('imgs/darkcity_bk.jpg'),side: THREE.DoubleSide}),
-            new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('imgs/darkcity_up.jpg'),side: THREE.DoubleSide}),
-            new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('imgs/desertsky_up.tga'),side: THREE.DoubleSide}),
-            new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('imgs/darkcity_rt.jpg'),side: THREE.DoubleSide}),
-            new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('imgs/darkcity_lf.jpg'),side: THREE.DoubleSide})  
+            new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('imgs/criminal-impact_ft.jpg'),side: THREE.DoubleSide}),
+            new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('imgs/criminal-impact_bk.jpg'),side: THREE.DoubleSide}),
+            new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('imgs/criminal-impact_up.jpg'),side: THREE.DoubleSide}),
+            new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('imgs/criminal-impact_dn.jpg'),side: THREE.DoubleSide}),
+            new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('imgs/criminal-impact_rt.jpg'),side: THREE.DoubleSide}),
+            new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('imgs/criminal-impact_lf.jpg'),side: THREE.DoubleSide})  
         ];
 
         /*var shader = THREE.ShaderLib["cube"];
@@ -23,11 +38,19 @@ class World extends THREE.Mesh{
             vertexShader  : shader.vertexShader,
             uniforms  : uniforms
         });*/
+        
 
-        this.material = new THREE.MeshFaceMaterial(this.texture);
-        this.world = new THREE.Mesh(this.geometry, this.material);
+        this.worldMaterial = new THREE.MeshFaceMaterial(this.texture);
+
+        this.world = new THREE.Mesh(this.geometry, this.worldMaterial);
+
+        this.building1Mesh = building1.toMesh();
+
+        this.building1Mesh.rotateY(Math.PI/4);
+        this.building1Mesh.position.set(0,25,-27);
 
         this.add(this.world);
+        this.add(this.building1Mesh);
     
     }
 
